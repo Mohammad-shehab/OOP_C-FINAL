dfb8c053a12a63de6fe9fb825b1a0bfb72a8f80e
Mohammad-shehab <q8mlg@outlook.com>
2024-12-26 10:02:39 +0300

    Add project files.

---
+++ ConsoleApp/Models/Product.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//class product
+//3 functions
+//first function takes name
+//second function takes name and price
+//third function takes name, price and quantity
+//print the values in the main function
+
+
+
+
+
+
+namespace ConsoleApp.Models
+{
+    public class Product
+    {
+
+
+
+        private string name;
+        private double price;
+        private int quantity;
+
+
+        private List<Product> products = new List<Product>();
+
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+
+        public void PrintName(string name)
+        {
+            Console.WriteLine("Name: " + name);
+        }
+
+        public void PrintNameAndPrice(string name, double price)
+        {
+            Console.WriteLine("Name: " + name + " ,Price: " + price);
+        }
+
+
+        public void PrintAll(string name, double price, int quantity)
+        {
+            Console.WriteLine("Name: " + name + " ,Price: " + price + " ,Quantity: " + quantity + " ,Total price: " + (quantity * price));
+
+        }
+
+
+        public void AddProduct(string name, double price, int quantity)
+        {
+            products.Add(new Product { Name = name, Price = price, Quantity = quantity });
+
+
+
+                 }
+
+
+        public void PrintAllProducts()
+        {
+            foreach (var x in products)
+            {
+                Console.WriteLine("Name: " + x.name + " ,Price: " + x.price + " ,Quantity: " + x.quantity + " ,Total price: " + (x.quantity * x.price));
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp.Models;
+
+
+//Console.WriteLine("product name ");
+string name="";
+
+//Console.WriteLine("product price ");
+double price=0;
+
+//Console.WriteLine("product quantity ");
+int quantity=0 ;
+
+
+Product product = new Product();
+
+
+//product.PrintName(name);
+//product.PrintNameAndPrice(name, price);
+//product.PrintAll(name, price, quantity);
+
+
+
+
+
+
+Console.WriteLine("Do you want to add a product? y/n");
+string  choice = Console.ReadLine().ToLower();
+while (choice == "y")
+{
+    Console.WriteLine("product name ");
+    name = Console.ReadLine();
+    Console.WriteLine("product price ");
+    price = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("product quantity ");
+    quantity = Convert.ToInt32(Console.ReadLine());
+    product.AddProduct(name, price, quantity);
+    Console.WriteLine("Do you want to add a product? y/n");
+    choice = Console.ReadLine().ToLower();
+}
+
+//product.PrintName(name);
+//product.PrintNameAndPrice(name, price);
+//product.PrintAll(name, price, quantity);
+product.PrintAllProducts();
